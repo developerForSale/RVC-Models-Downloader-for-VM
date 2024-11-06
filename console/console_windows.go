b15c81e0fb30e115504d275e5bc77b5e683f0185
Zhenghang.Huang <huang@eggdemy.com>
2024-11-06 12:05:22 +0800

    Comment out the stdout mode settings statements.

--- console/console_windows.go
+++ console/console_windows.go
@@ -63,16 +63,16 @@ func init() {
 		panic(err)
 	}
 
-	stdout := windows.Handle(os.Stdout.Fd())
-	err = windows.GetConsoleMode(stdout, &mode)
-	if err != nil {
-		panic(err)
-	}
+	// stdout := windows.Handle(os.Stdout.Fd())
+	// err = windows.GetConsoleMode(stdout, &mode)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	mode |= windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING // 启用虚拟终端处理
-	mode |= windows.ENABLE_PROCESSED_OUTPUT            // 启用处理后的输出
+	// mode |= windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING // 启用虚拟终端处理
+	// mode |= windows.ENABLE_PROCESSED_OUTPUT            // 启用处理后的输出
 
-	err = windows.SetConsoleMode(stdout, mode)
+	// err = windows.SetConsoleMode(stdout, mode)
 	// windows 带颜色 log 自定义格式
 	logrus.SetFormatter(&logFormat{hasColor: err == nil})
 	if err != nil {
